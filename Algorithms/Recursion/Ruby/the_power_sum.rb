c58a77d3a082c260a1d2d98e64c456be264c8a5e
obcilion <raymond@kolabnow.com>
2018-01-21 00:24:10 +0100

    :truck: Changed folder structure, added file

---
+++ Algorithms/Recursion/Ruby/the_power_sum.rb
@@ -0,0 +1,11 @@
+#!/bin/ruby
+#https://www.hackerrank.com/challenges/the-power-sum/problem
+
+def powerSum(x, n)
+    # Complete this function
+end
+
+x = gets.strip.to_i
+n = gets.strip.to_i
+result = powerSum(X, N)
+puts result
